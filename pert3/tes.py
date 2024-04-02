1aba795d40e1cb293bd6c3d779f6169c196174cd
itsmeyzee <meyzehadishopia@gmail.com>
2024-04-02 10:53:55 +0700

    first commit

---
+++ pert3/tes.py
@@ -0,0 +1,13 @@
+value = int(input("enter value :"))
+if value >= 90:
+    grade = "A"
+elif value >= 80:
+    grade = "B"
+elif value >= 70:
+    grade = "C"
+elif value >= 60:
+    grade = "D"
+else :
+    grade = "E"
+
+print("Grade : %s" % grade)
